e613141430029ff595f71d79fa99a27d510528fc
Kres <53458633+kresdian@users.noreply.github.com>
2019-08-12 15:32:08 +0700

    Update Budget.js

--- src/views/Dashboard/components/Budget/Budget.js
+++ src/views/Dashboard/components/Budget/Budget.js
@@ -7,7 +7,7 @@ import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import MoneyIcon from '@material-ui/icons/Money';
 import VideocamIcon from '@material-ui/icons/Videocam';
 import { NavLink } from 'react-router-dom';
-
+import FavoriteIcon from '@material-ui/icons/favorite';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -76,7 +76,7 @@ const Budget = props => {
           </Grid>
         </Grid>
         <div className={classes.difference}>
-          <ArrowDownwardIcon className={classes.differenceIcon} />
+          <FavoriteIcon className={classes.differenceIcon} />
           <Typography
             className={classes.differenceValue}
             variant="body2"
