ed457549931a1193c76f22cb2ece65c304636c9f
Kres <53458633+kresdian@users.noreply.github.com>
2019-08-20 12:12:12 +0700

    Update Yayan.js

--- src/views/Yayan/Yayan.js
+++ src/views/Yayan/Yayan.js
@@ -16,11 +16,54 @@ import tileData from './tileData';
 
 
 
-const useStyles = makeStyles({
+GridListTile. The overlay can accommodate a title, subtitle and secondary action - in this example an IconButton.
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import tileData from './tileData';
+
+const useStyles = makeStyles(theme => ({
   root: {
-   
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
   },
-});
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+}));
+
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
+export default function TitlebarGridList() {
+  const classes = useStyles();
 
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
