112b172d9e28ce4cf0143a6b0d3d5f1b25a78bad
Kres <53458633+kresdian@users.noreply.github.com>
2019-08-20 11:48:52 +0700

    Update Yayan.js

--- src/views/Yayan/Yayan.js
+++ src/views/Yayan/Yayan.js
@@ -1,20 +1,4 @@
 
-Contents
-
-Bottom Navigation
-Bottom Navigation with no label
-API
-Bottom Navigation
-Bottom navigation bars allow movement between primary destinations in an app.
-Help us keep running
-If you don't mind tech related ads (no tracking or remarketing), and want to keep us running, please whitelist Material-UI in your blocker.
-Thank you! ❤️
-
-Bottom navigation bars display three to five destinations at the bottom of a screen. Each destination is represented by an icon and an optional text label. When a bottom navigation icon is tapped, the user is taken to the top-level navigation destination associated with that icon.
-
-Bottom Navigation
-When there are only three actions, display both icons and text labels at all times.
-
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
