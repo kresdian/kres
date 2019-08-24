2ac6805168f20d65ce86ee1d6b1ef053321c3803
Kres <53458633+kresdian@users.noreply.github.com>
2019-08-24 12:51:33 +0700

    Update Sidebar.js

--- src/layouts/Main/components/Sidebar/Sidebar.js
+++ src/layouts/Main/components/Sidebar/Sidebar.js
@@ -74,28 +74,7 @@ const Sidebar = props => {
    
   ];
 
-  return (
-    <Drawer
-      anchor="left"
-      classes={{ paper: classes.drawer }}
-      onClose={onClose}
-      open={open}
-      variant={variant}
-    >
-      <div
-        {...rest}
-        className={clsx(classes.root, className)}
-      >
-        <Profile />
-        <Divider className={classes.divider} />
-        <SidebarNav
-          className={classes.nav}
-          pages={pages}
-        />
-        <UpgradePlan />
-      </div>
-    </Drawer>
-  );
+  
 };
 
 Sidebar.propTypes = {
