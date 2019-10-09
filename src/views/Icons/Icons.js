91a64006e94730691b198076595ff0d3d4111881
Kres <53458633+kresdian@users.noreply.github.com>
2019-10-09 20:04:07 +0700

    Update Icons.js

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -25,9 +25,10 @@ return (
           Paper can be used to build surface or other elements for your application.
         </Typography>
 
+<div class="gcse-search"></div>
 
-<script> (function() { var cx = '0089289843748653:ikyblft4'; var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true; gcse.src = 'https://cse.google.com/cse.js?cx=' + cx; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s); })(); </script> <gcse:search></gcse:search>
-      </Paper>
+  
+  </Paper>
     </div>
   );
 }
