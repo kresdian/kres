989fab08a0beb3cb194de87bbcea78100fb645f5
Kres <53458633+kresdian@users.noreply.github.com>
2019-10-09 13:16:21 +0700

    Update Icons.js

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -1,30 +1,71 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(4)
+    padding: theme.spacing(3, 2),
   },
-  iframe: {
-    width: '100%',
-    minHeight: 640,
-    border: 0
-  }
 }));
 
-const Icons = () => {
+export default function PaperSheet() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <div>
+    <div>
+      <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
+      </Paper>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+  },
+}));
+
+export default function Icons() {
+  const classes = useStyles();
+
+
+return (
+    <div>
+      <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper.
 <script async src="https://cse.google.com/cse.js?cx=014391522214403407077:7d0gelgeawr"></script>
 <div class="gcse-search"></div>
 
 
-</div>
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
+      </Paper>
     </div>
   );
-};
+}
+
 
 export default Icons;
+
+
+
