b259bf7f301676b15b49ca2bcef7d6356f2b0779
Kres <53458633+kresdian@users.noreply.github.com>
2019-10-09 21:05:16 +0700

    Update Icons.js

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -1,70 +1,22 @@
 
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-
-const useStyles = makeStyles(theme => ({
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-}));
-
-
-
+import Container from '@material-ui/core/Container';
 const Icons = () => {
   const classes = useStyles();
 
-return (
-  
-  <Card className={classes.card}>
-      <CardHeader
-        action={
-          SEARCH     
-  }
-        action={
-          <div class="gcse-search"></div>       
-  }
-      />
-      
-  
-        
-
-  
-    </Card>
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+         <div class="gcse-search"></div>       
+      </Container>
+    </React.Fragment>
   );
 }
   
+
   
 
 
