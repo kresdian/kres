593331dd7272cf128bb555699ae4114df2506744
Kres <53458633+kresdian@users.noreply.github.com>
2019-10-09 13:31:35 +0700

    Update Icons.js

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -18,9 +18,6 @@ return (
       <Paper className={classes.root}>
         <Typography variant="h5" component="h3">
           This is a sheet of paper.
-<script async src="https://cse.google.com/cse.js?cx=014391522214403407077:7d0gelgeawr"></script>
-<div class="gcse-search"></div>
-
 
         </Typography>
         <Typography component="p">
@@ -29,7 +26,7 @@ return (
 <form method = "get" title = "Search Form" action="https://cse.google.com/cse/publicurl">
  <div>
     <input type="text" id="q" name="q" title="Search this site" alt="Search Text" maxlength="256" />
-    <input type="hidden" id="cx" name="cx" value="013626029654558379071:ze3tw4csia4" />
+    <input type="hidden" id="cx" name="cx" value="014391522214403407077:7d0gelgeawr" />
    <input type="image" id="searchSubmit" name="submit" src="https://www.flaticon.com/free-icon/active-search-symbol_34148" alt="Go" title="Submit Search Query" />
  </div>
 </form>
