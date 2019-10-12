6c653a5e536489326a1dbc2894804ba07584066b
Kres <53458633+kresdian@users.noreply.github.com>
2019-10-12 22:39:35 +0700

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -31,7 +31,7 @@ const Routes = () => {
         to="/search"
       />
       <RouteWithLayout
-        component={DashboardView}
+        component={SearchView}
         exact
         layout={MainLayout}
         path="/search"
