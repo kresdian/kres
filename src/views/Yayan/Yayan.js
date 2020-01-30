e86eff80d82da62b6f924781ffb78731b1382be1
Kres <53458633+kresdian@users.noreply.github.com>
2020-01-30 14:53:40 +0700

    Update Yayan.js

--- src/views/Yayan/Yayan.js
+++ src/views/Yayan/Yayan.js
@@ -1,50 +1,17 @@
-
 import React from 'react';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import { Card, CardContent, Grid, Typography, Avatar } from '@material-ui/core';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import MoneyIcon from '@material-ui/icons/Money';
-import VideocamIcon from '@material-ui/icons/Videocam';
-import { NavLink } from 'react-router-dom';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import BookmarksIcon from '@material-ui/icons/Bookmarks';
-import NearMeIcon from '@material-ui/icons/NearMe';
-
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    height: '100%'
+    flexGrow: 1,
   },
-  content: {
-    alignItems: 'center',
-    display: 'flex'
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
   },
-  title: {
-    fontWeight: 700
-  },
-  avatar: {
-    backgroundColor: theme.palette.warning.main,
-    height: 56,
-    width: 56
-  },
-  icon: {
-    height: 32,
-    width: 32
-  },
-  difference: {
-    marginTop: theme.spacing(2),
-    display: 'flex',
-    alignItems: 'center'
-  },
-  differenceIcon: {
-    color: theme.palette.white
-  },
-  differenceValue: {
-    color: theme.palette.white,
-    marginRight: theme.spacing(1)
-  }
 }));
 
 const Yayan = props => {
@@ -53,65 +20,31 @@ const Yayan = props => {
   const classes = useStyles();
 
   return (
-    <Card
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      <CardContent>
-        <Grid
-          container
-          justify="space-between"
-        >
-          <Grid item>
-            <Typography
-              className={classes.title}
-              color="textSecondary"
-              gutterBottom
-              variant="h5"
-            >
-           <NavLink to="/tv">Garangan Video</NavLink>
-            </Typography>
-            <Typography variant="body2">Streaming video</Typography>
-          </Grid>
-          <Grid item>
-            <Avatar className={classes.avatar}>
-              <VideocamIcon />
-    </Avatar>
-          </Grid>
+   
+      <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
         </Grid>
-        <div className={classes.difference}>
-        
-<NavLink to="/music">
-        
-  <BookmarksIcon /></NavLink>
-            <Typography
-            className={classes.captionValue}
-            variant="h6"
-          >
-            <NavLink to="/music">Detail</NavLink>
-           
-          </Typography>
-          <Typography
-            className={classes.caption}
-            variant="h6"
-          >
-            &emsp; &emsp;
-          </Typography>
-&emsp;
-<NavLink to="/music">
-        
-  <img src="https://img.icons8.com/material-outlined/24/000000/near-me.png"/></NavLink>
-         
-         <Typography
-            className={classes.differenceValue}
-            variant="h6"
-          >
-            <a href="http://send.yplay.online">Buka</a>
-           
-          </Typography>
-        </div>
-      </CardContent>
-    </Card>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
