59992fecd0edc5a6c91b124a98af59294dd2d52a
Kres <53458633+kresdian@users.noreply.github.com>
2020-01-30 14:39:41 +0700

    Update Yayan.js

--- src/views/Yayan/Yayan.js
+++ src/views/Yayan/Yayan.js
@@ -1,52 +1,123 @@
+
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { Card, CardContent, Grid, Typography, Avatar } from '@material-ui/core';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import MoneyIcon from '@material-ui/icons/Money';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import { NavLink } from 'react-router-dom';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import BookmarksIcon from '@material-ui/icons/Bookmarks';
+import NearMeIcon from '@material-ui/icons/NearMe';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
+    height: '100%'
+  },
+  content: {
+    alignItems: 'center',
+    display: 'flex'
+  },
+  title: {
+    fontWeight: 700
+  },
+  avatar: {
+    backgroundColor: theme.palette.warning.main,
+    height: 56,
+    width: 56
+  },
+  icon: {
+    height: 32,
+    width: 32
+  },
+  difference: {
+    marginTop: theme.spacing(2),
     display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
+    alignItems: 'center'
   },
-  gridList: {
-    width: 500,
-    height: 450,
+  differenceIcon: {
+    color: theme.palette.white
   },
+  differenceValue: {
+    color: theme.palette.white,
+    marginRight: theme.spacing(1)
+  }
 }));
 
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *     cols: 2,
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- */
-export default function ImageGridList() {
+const Yayan = props => {
+  const { className, ...rest } = props;
+
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <GridList cellHeight={160} className={classes.gridList} cols={3}>
-        (
-          <GridListTile key={tile.img} cols={tile.cols || 1}>
-            <img src={tile.img} alt={tile.title} />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
+    <Card
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <CardContent>
+        <Grid
+          container
+          justify="space-between"
+        >
+          <Grid item>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+              variant="h5"
+            >
+           <NavLink to="/tv">Garangan Video</NavLink>
+            </Typography>
+            <Typography variant="body2">Streaming video</Typography>
+          </Grid>
+          <Grid item>
+            <Avatar className={classes.avatar}>
+              <VideocamIcon />
+    </Avatar>
+          </Grid>
+        </Grid>
+        <div className={classes.difference}>
+        
+<NavLink to="/music">
+        
+  <BookmarksIcon /></NavLink>
+            <Typography
+            className={classes.captionValue}
+            variant="h6"
+          >
+            <NavLink to="/music">Detail</NavLink>
+           
+          </Typography>
+          <Typography
+            className={classes.caption}
+            variant="h6"
+          >
+            &emsp; &emsp;
+          </Typography>
+&emsp;
+<NavLink to="/music">
+        
+  <img src="https://img.icons8.com/material-outlined/24/000000/near-me.png"/></NavLink>
+         
+         <Typography
+            className={classes.differenceValue}
+            variant="h6"
+          >
+            <a href="http://send.yplay.online">Buka</a>
+           
+          </Typography>
+        </div>
+      </CardContent>
+    </Card>
   );
-}
+};
+
+Yayan.propTypes = {
+  className: PropTypes.string
+};
+
+export default Yayan;
+
