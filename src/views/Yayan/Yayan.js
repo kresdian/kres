10fe668f61ad4731d2ca2215be1bed027ffdd8e7
Kres <53458633+kresdian@users.noreply.github.com>
2020-01-30 15:25:21 +0700

    Update Yayan.js

--- src/views/Yayan/Yayan.js
+++ src/views/Yayan/Yayan.js
@@ -7,7 +7,7 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
-import tileData from './tileData';
+import titleData from './titleData';
 
 const useStyles = makeStyles(theme => ({
   root: {
